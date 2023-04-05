8244cf2332f0351b847c6b642d5da1cf79fc32a7
eckstein75 <mauroeckstein@hotmail.com>
2023-04-05 18:14:21 -0300

    colocando .env

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,4 @@
-import { Router } from "express";
+import { Router } from 'express';
 import * as PagesController from '../controllers/pageController';
 import * as SearchController from '../controllers/searchController';
 
